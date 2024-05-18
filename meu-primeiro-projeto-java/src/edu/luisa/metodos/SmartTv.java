f33735247e4639cff7630a88b51999f4e9a736b7
Luisa Lemos <luisa_dluck@hotmail.com>
2024-05-18 13:14:41 -0300

    terceiro commit

---
+++ meu-primeiro-projeto-java/src/edu/luisa/metodos/SmartTv.java
@@ -0,0 +1,41 @@
+package edu.luisa.metodos;
+
+/*Vamos criar um exemplo de uma classe para representar uma SmartTV onde:
+
+Ela tenha as características: ligada (boolean), canal (int) e volume (int);
+Nossa TV poderá ligar e desligar e assim mudar o estado ligada;
+Nossa TV aumentará e diminuirá o volume sempre em +1 ou -1;
+Nossa TV poderá mudar de canal de 1 em 1 ou definindo o número correspondente.*/
+
+public class SmartTv {
+   boolean  ligada=false;
+   int canal=1;
+   int volume=25;
+
+   public void mudarCanal(int novoCanal){
+      canal = novoCanal;
+   }
+
+   public void aumentarCanal(){
+      canal++;
+   }
+   public void diminuirCanal(){
+      canal--;
+   }
+
+   public void ligar(){
+      ligada=true;
+   }
+   public void desligar(){
+      ligada=false;
+   }
+
+   public void aumentarVolume(){
+      volume++;
+      System.out.println("Aumentando volume para: " + volume);
+   }
+   public void diminuirVolume(){
+      volume--;
+      System.out.println("Diminuindo volume para: " + volume);
+   }
+}
--- meu-primeiro-projeto-java/src/edu/luisa/operadores/Operadores.java
+++ meu-primeiro-projeto-java/src/edu/luisa/operadores/Operadores.java
@@ -5,10 +5,10 @@ import java.util.Date;
 public class Operadores {
     public static void main(String[] args) throws Exception {
         /*Atribuição
-        String nome = "GLEYSON";
+        String nome = "LUISA";
         int idade = 22;
         double peso = 68.5;
-        char sexo = 'M';
+        char sexo = 'F';
         boolean doadorOrgao = false;
         Date dataNascimento = new Date();
 
@@ -61,6 +61,66 @@ public class Operadores {
         boolean verdadeiro = true;
 
         System.out.println("Inverteu " + !verdadeiro);
-        */
-    }
-}
+        
+
+        //Ternários
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = (a==b) ? "verdadeiro" : "false";
+
+        System.out.println(resultado);
+
+        //Relacionais
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if(numero1 > numero2)
+            System.out.print("Numero 1 maior que numero 2");
+
+        if(numero1 < numero2)
+            System.out.println("Numero 1 menor que numero 2");
+
+        if(numero1 >= numero2)
+            System.out.println("Numero 1 maior ou igual que numero 2");
+
+        if(numero1 <= numero2)
+            System.out.println("Numero 1 menor ou igual que numero 2");
+
+        if(numero1 != numero2)
+            System.out.println("Numero 1 é diferente de numero 2");
+
+        String nome1 = "JAVA";
+        String nome2 = "JAVA";
+
+        System.out.println(nome1 == nome2); // true
+
+        String nome3 = new String("JAVA");
+
+        System.out.println(nome1 == nome3); // false
+
+        String nome4 = nome3;
+
+        System.out.println(nome3 == nome4); // true
+
+        System.out.println(nome1.equals(nome2)); 
+        System.out.println(nome2.equals(nome3)); 
+        System.out.println(nome3.equals(nome4)); 
+
+        //Logicos
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        if(condicao1 && condicao2)
+            System.out.print("Os dois valores precisam ser verdadeiros");
+
+
+        if(condicao1 || condicao2)
+            System.out.print("Um dos valores precisa ser verdadeiro");*/
+
+            }
+        }
