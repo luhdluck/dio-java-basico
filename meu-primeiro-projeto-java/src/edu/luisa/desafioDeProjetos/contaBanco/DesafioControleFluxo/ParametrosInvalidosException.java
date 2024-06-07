e338c37ae02dbf224fa4f66b24f4cfdc78ea6650
Luisa Lemos <luisa_dluck@hotmail.com>
2024-06-06 21:07:16 -0300

    sexto commit

---
+++ meu-primeiro-projeto-java/src/edu/luisa/desafioDeProjetos/contaBanco/DesafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package edu.luisa.desafioDeProjetos.contaBanco.DesafioControleFluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
--- meu-primeiro-projeto-java/src/edu/luisa/desafioDeProjetos/contaBanco/contador/Contador.java
+++
@@ -1,5 +0,0 @@
-package edu.luisa.desafioDeProjetos.contaBanco.contador;
-
-public class Contador {
-    
-}
